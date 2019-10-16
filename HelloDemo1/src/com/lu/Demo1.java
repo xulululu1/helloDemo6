6ac739538cb344b2bfc8bf5fba101d7f4bcbdac6
xulu_a <lgoodMorning_pro@atguigu.com>
2019-10-16 08:56:32 +0800

    222

--- HelloDemo1/src/com/lu/Demo1.java
+++ HelloDemo1/src/com/lu/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("aaaaaaaa");
         System.out.println("aaaaaaaaaaaaa");
+        System.out.println("ssssssssss");
     }
 }
